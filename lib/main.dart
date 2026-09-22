e984464f0110a301328158f2cbb4c8d9bd917230
Katrisbug <bonetkatrina@gmail.com>
2026-09-22 16:22:30 -0300

    iniciando

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,21 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:sigpe/views/home.dart';
 
-void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const App()));
+void main(){
+  runApp(DevicePreview(builder: (context) => App()));
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  // This widget is the root of your application.
+class SigpeApp extends StatelessWidget {
+  const SigpeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'SIGPE',
+      home: const LoginPage(),
     );
   }
 }
