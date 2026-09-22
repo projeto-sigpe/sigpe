6029da093b0b1e77227f73cf0f01c2c1be1d72df
LopesSoh <sophiac.lopes2008@gmail.com>
2026-09-22 14:32:21 -0300

    device preview e pastas base

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => const App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   // This widget is the root of your application.
   @override
